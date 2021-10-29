3a78bd6843ea179768eb4c2415762cb386b5c42a
runba3011 <runba3011@gmail.com>
2021-10-29 10:14:54 +0900

    バリデーションの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :point
+    validates :lank
+    validates :experience_point , numericality: {only_integer: true}
+  end
 end
