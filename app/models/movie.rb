e7389fffc442e8b8e1649762754cb67824959a31
runba3011 <runba3011@gmail.com>
2021-10-29 11:52:41 +0900

    バリデーションの追加

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,8 @@
 class Movie < ApplicationRecord
+  attr_accessor :is_confirm
+  with_options presence: true do
+    validates :title 
+    validates :link , length: {is: 11}
+    validates :single_point , numericality: {only_integer: true , greater_than_or_equal_to: 1 , less_than_or_equal_to: 5 }
+  end
 end
