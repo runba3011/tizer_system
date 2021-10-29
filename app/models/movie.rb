b53b3214b54b4f2f85f4e641adb512f94aa5a355
runba3011 <runba3011@gmail.com>
2021-10-29 13:25:09 +0900

    動画視聴ページで投稿者の名前が表示されるように変更

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,7 +2,8 @@ class Movie < ApplicationRecord
   attr_accessor :is_confirm
   with_options presence: true do
     validates :title 
-    validates :link , length: {is: 11} , uniqueness: true
+    validates :link , length: {is: 11}
     validates :single_point , numericality: {only_integer: true , greater_than_or_equal_to: 1 , less_than_or_equal_to: 5 }
   end
+  belongs_to :user
 end
