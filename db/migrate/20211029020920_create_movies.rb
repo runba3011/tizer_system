a9673a4eb100e438a901d3eacbeb4f43dd821464
runba3011 <runba3011@gmail.com>
2021-10-29 11:13:15 +0900

    カラムの設定

--- db/migrate/20211029020920_create_movies.rb
+++ db/migrate/20211029020920_create_movies.rb
@@ -1,7 +1,9 @@
 class CreateMovies < ActiveRecord::Migration[6.0]
   def change
     create_table :movies do |t|
-
+      t.string :link , null: false
+      t.integer :single_point , null: false 
+      t.references :user , null: false , foreign_key: true
       t.timestamps
     end
   end
