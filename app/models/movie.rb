7a8c578694e718e1f18656ef0f5e823ece2857b2
runba3011 <runba3011@gmail.com>
2021-10-29 13:23:33 +0900

    既に投稿されている動画を投稿できないように変更

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,7 +2,7 @@ class Movie < ApplicationRecord
   attr_accessor :is_confirm
   with_options presence: true do
     validates :title 
-    validates :link , length: {is: 11}
+    validates :link , length: {is: 11} , uniqueness: true
     validates :single_point , numericality: {only_integer: true , greater_than_or_equal_to: 1 , less_than_or_equal_to: 5 }
   end
 end
