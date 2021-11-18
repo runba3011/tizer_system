3601132b20b65fe2226510d666c9f0dffab69eac
runba3011 <runba3011@gmail.com>
2021-11-18 13:44:09 +0900

    部分テンプレート表示の設定

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,6 +1,7 @@
 class Material < ActiveHash::Base
   self.data = [
     {id: 1, name: 'リンク1' , image: "material/templete/material1.png"},
-    {id: 2, name: 'リンク2' , image: "material/templete/material2.png"}
+    {id: 2, name: 'リンク2' , image: "material/templete/material2.png"},
+    {id: 3, name: 'リンク3' , image: "material/templete/material3.png"},
   ]
 end
